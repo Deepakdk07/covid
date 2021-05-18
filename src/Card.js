9d469e08982a3511780abd73246c916fc409e442
Deepak Kumar <deadlydeepak9@gmail.com>
2021-05-18 13:00:41 +0530

    % added

--- src/Card.js
+++ src/Card.js
@@ -37,10 +37,12 @@ const Card = ({data, stateCode, input}) => {
      {data.delta ?  <p>Today's Confirmed : {data.delta && data.delta.confirmed}</p> : null }
      {data.delta ?  <p>Today's deaths : {data.delta && data.delta.deceased}</p> : null }
      {data.delta ?  <p>Today's recovered : {data.delta && data.delta.recovered}</p> : null }
+     {data.delta ?  <p>Today's recovery : {data.delta && data.delta.recovered}</p> : null }
       <p>Total Confirmed Cases  : {data.total && data.total.confirmed}</p>
       <p>Total Deaths Recorded : {data.total && data.total.deceased}</p>
       <p>Total Recovered Patients : {data.total && data.total.recovered}</p>
       <p>Total Tested Person : {data.total && data.total.tested}</p>
+      <p>Total confirmed cases Rate : {data.total && data.total.confirmed/data.total && data.total.tested * 100}</p>
       <p>Total Vaccinated : {data.total && data.total.vaccinated}</p>
       {/* <hr /> */}
      </div>
